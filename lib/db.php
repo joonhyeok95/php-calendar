571f56d2a6377857fbd3d24cd6a6e19961d5a102
joonhyeok95 <dkttkemf@gmail.com>
2025-11-05 00:20:49 +0900

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,10 +1,15 @@
 <?php
-    $mysqli = new mysqli('localhost', 'root', '', 'yehan');
-    if ($mysqli->connect_errno) {
-        echo json_encode(['status' => 'error', 'message' => 'DB 연결 실패']);
-        exit;
-    }
-    // UTF-8 인코딩 설정 (한글 등 다국어 처리 용)
-    $mysqli->set_charset("utf8");
 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "yehan";
+
+$mysqli = new mysqli($host, $user, $pass, $dbname);
+if ($mysqli->connect_errno) {
+    echo json_encode(['status' => 'error', 'message' => 'DB 연결 실패']);
+    exit;
+}
+// UTF-8 인코딩 설정 (한글 등 다국어 처리 용)
+$mysqli->set_charset("utf8");
 ?>
